6ff5b5135c2cd771c657991ecc3f7b5ba745fb74
NataliaOrtizMa <nataliaortizma@gmail.com>
2021-07-22 21:07:04 -0500

    Layout integration

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,19 +18,19 @@ const Header = () => {
             <header>
                 <nav class="navbar navbar-expand-lg navbar-light">
                     <div class="container-fluid">
-                        <a class="navbar-brand" href="#">Centralizador de Indicadores</a>
+                        <a class="navbar-brand" href="/">Centralizador de Indicadores</a>
                         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                         </button>
-                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="/registro">Registrarse</a>
-                            </li>
-                            <li class="nav-item">
-                            <a class="nav-link" href="/login">Ingresar</a>
-                            </li>
-                        </ul>
+                        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                            <ul class="navbar-nav  ">
+                                <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="/registro">Registrarse</a>
+                                </li>
+                                <li class="nav-item">
+                                <a class="nav-link" href="/login">Ingresar</a>
+                                </li>
+                            </ul>
                         </div>
                     </div>
                 </nav>
