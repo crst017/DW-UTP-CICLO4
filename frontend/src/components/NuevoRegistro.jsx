cd22d794302661c4833f430cf713d3ce20c2a8f8
cristian navarro leon <cristiannl.0117@gmail.com>
2021-07-24 16:38:19 -0500

    modificar estilos

--- frontend/src/components/NuevoRegistro.jsx
+++ frontend/src/components/NuevoRegistro.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './NuevoRegistro.css'
 
 const NuevoRegistro = () => {
+
     return (
 
         <div className="container-fluid col-6 my-5 registro">
@@ -9,15 +10,28 @@ const NuevoRegistro = () => {
         <h2>Registrar nuevo indicador</h2>
         <section class="container-fluid card my-5 g-0 p-4 px-5 registro">
 
-            <label class="font-weight-bold mb-0">Empresa</label>
-            <select class="form-select solo" aria-label="Default select example">
-                <option selected>Empresa</option>
-                <option value="2021">Empresa A</option>
-                <option value="2020">Empresa B</option>
-                <option value="2019">Empresa C</option>
-                <option value="2018">Empresa D</option>
-            </select>
+            <h3 class="card-header font-weight-bold mt-2 mb-4 text-center">Empresa</h3>
             
+            <div className="doble-col">
+            <label class="font-weight-bold mb-0 mt-2">Servicio
+            <select class="form-select" aria-label="Default select example">
+                <option selected>Servicio</option>
+                <option value="2021">Telefonia</option>
+                <option value="2020">Internet</option>
+                <option value="2019">Television</option>
+            </select>
+            </label>
+
+            <label class="font-weight-bold mb-0 mt-2">Indicador
+            <select class="form-select" aria-label="Default select example">
+                <option selected>Indicador</option>
+                <option value="2021">Retencion clientes</option>
+                <option value="2020">Peticiones resueltas</option>
+                <option value="2019">Solución mesa ayuda</option>
+            </select>
+            </label>
+            </div>
+
             <div className="doble-col">
                 
                 <label class="font-weight-bold mb-0">Año
@@ -47,52 +61,16 @@ const NuevoRegistro = () => {
                 <option value="diciembre">Diciembre</option>
             </select>
             </label>
-            </div>
-
-            <div className="doble-col">
-            <label class="font-weight-bold mb-0 mt-2">Servicio
-            <select class="form-select" aria-label="Default select example">
-                <option selected>Servicio</option>
-                <option value="2021">Telefonia</option>
-                <option value="2020">Internet</option>
-                <option value="2019">Television</option>
-            </select>
-            </label>
-
-            <label class="font-weight-bold mb-0 mt-2">Indicador
-            <select class="form-select" aria-label="Default select example">
-                <option selected>Indicador</option>
-                <option value="2021">Retencion clientes</option>
-                <option value="2020">Peticiones resueltas</option>
-                <option value="2019">Solución mesa ayuda</option>
-            </select>
-            </label>
-            </div>
+            </div> 
             
-            <label class="font-weight-bold solo">¿Fue multado?
-                <select class="form-select" aria-label="Default select example">
-                    <option selected>Multa</option>
-                    <option value="2021">Si</option>
-                    <option value="2020">No</option>
-                </select>
-            </label>
-
-            <div className="doble-col">
+            <div className="w-50">
                 <label class="font-weight-bold">% Cumplimiento
                     <div className="form-floating">
                         <input type="email" className="form-control" id="floatingEmail" placeholder="0% - 100%" required></input>
                         <label htmlFor="floatingEmail">Cumplimiento</label>
                     </div>
                 </label>
-
-                <label class="font-weight-bold">Valor multa
-                <div className="form-floating">
-                    <input type="email" className="form-control" id="floatingEmail" placeholder="1000000" required></input>
-                    <label htmlFor="floatingEmail">Valor multa</label>
-                </div>
-                </label>
             </div>
-
 
             <label for="exampleFormControlTextarea1">Comentarios</label>
             <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
